cb923ac4e1072bba619c71ba9929095f003c7d1e
arash-khd <126689940+arash-khd@users.noreply.github.com>
2023-07-13 04:58:46 +0330

    Update tags.php

--- tags.php
+++ tags.php
@@ -1,8 +1,12 @@
 <?php $tags = get_the_tags();
-foreach ($tags as $tag) {
 
-  echo "<a href='$tag->slug'> $tag->name <span> , </span> </a>";
+if(!empty($tags)){ ?>
+<div class="post-tags d-flex">
+<?php foreach ($tags as $tag) {
+$tag_slug = esc_attr( get_tag_link( $tag->term_id ) );	
+  echo "<a href='$tag_slug'> $tag->name <span> , </span> </a>";
   
 }
 
- ?>
+ ?> </div>
+<?php } ?>
